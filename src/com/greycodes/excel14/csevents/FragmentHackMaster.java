aa726c604a9417a48124004d3566ccd46e8424fc
itheark <itheark@gmail.com>
2014-08-10 01:02:21 +0530

    splashscreen n textcorrection

--- src/com/greycodes/excel14/csevents/FragmentHackMaster.java
+++ src/com/greycodes/excel14/csevents/FragmentHackMaster.java
@@ -49,7 +49,8 @@ public class FragmentHackMaster extends SherlockFragment implements OnClickListe
 		
 		if(v.equals(intro))
 		{
-			updateTextValue("ï¿½Most hackers are young because young people tend to be adaptable. As long as you remain adaptableï¿½\nwhatï¿½s life without a certain scandal? Whatï¿½s power without some mischief? Living at the edge of the law? Be the vigilante that controls the code.\n\n" );
+			updateTextValue("“Most hackers are young because young people tend to be adaptable. As long as you remain adaptable”\nwhat’s life without a certain scandal? What’s power without some mischief? Living at the edge of the law? Be the vigilante that controls the code.\n\n" );
+
 			
 		}
 		if (v.equals(rules))
