54e7720a2b558d4b5b607a0e1bd01954f1a08545
itheark <itheark@gmail.com>
2014-08-11 01:56:41 +0530

    Uichanged

--- src/com/greycodes/excel14/login/SigninActivity.java
+++ src/com/greycodes/excel14/login/SigninActivity.java
@@ -1,22 +1,119 @@
 package com.greycodes.excel14.login;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.facebook.android.DialogError;
+import com.facebook.android.Facebook;
+import com.facebook.android.FacebookError;
+import com.facebook.android.Util;
+import com.facebook.android.Facebook.DialogListener;
 import com.greycodes.excel14.R;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class SigninActivity extends Activity {
-
+public class SigninActivity extends Activity implements OnClickListener {
+	
+	
+	Facebook fb;
+	
+	SharedPreferences sp;
+	String id;
+	ImageView connectfb;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.user_registration_activity);
+		connectfb= (ImageView)findViewById(R.id.fbconnect);
+		
+		String APP_ID ="1404508176479531";
+		fb =new Facebook(APP_ID);
+		sp =getPreferences(MODE_PRIVATE);
+		
+		String access_token = sp.getString("access_token", null);
+		long expires =sp.getLong("access_expires",0);
+		
+		if (access_token!=null)
+		{
+			fb.setAccessToken(access_token);
 			
+		}
+		if (expires!=0)
+		{
+			fb.setAccessExpires(expires);
+		}
+		
+		
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+		StrictMode.setThreadPolicy(policy);
+		connectfb.setOnClickListener(this);
+		update();	
+		
 		
 	}
 	
 	
+
+	private void update() {
+		// TODO Auto-generated method stub
+		
+		if(fb.isSessionValid())
+		{
+			
+			JSONObject obj=null;
+			
+		
+			
+				
+				try {
+					String jsonUser =fb.request("me");
+					obj = Util.parseJson(jsonUser);
+					String id =obj.optString("id");
+				
+					
+				} catch (FacebookError e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				
+			
+		
+		
+		}
+		
+		
+		
+		
+		
+	}
+
+
 
 	@Override
 	protected void onPause() {
@@ -24,6 +121,59 @@ public class SigninActivity extends Activity {
 		super.onPause();
 		
 		}
+
+
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+
+		// TODO Auto-generated method stub
+	if(v.equals(connectfb)){	
+		if (fb.isSessionValid())
+		{
+		}
+		else 
+		{
+			fb.authorize(SigninActivity.this, new String[] {"email"} ,Facebook.FORCE_DIALOG_AUTH,new DialogListener() {
+				
+				@Override
+				public void onFacebookError(FacebookError e) {
+					// TODO Auto-generated method stub
+					Toast.makeText(SigninActivity.this,"FBerror",Toast.LENGTH_SHORT).show();
+				}
+				
+				@Override
+				public void onError(DialogError e) {
+					// TODO Auto-generated method stub
+					Toast.makeText(SigninActivity.this,"Error",Toast.LENGTH_SHORT).show();
+				}
+				
+				@Override
+				public void onComplete(Bundle values) {
+					// TODO Auto-generated method stub
+					
+					update();
+					Toast.makeText(SigninActivity.this,"Logged IN",Toast.LENGTH_LONG).show();
+					
+							
+					
+					
+					
+					
+				}
+				
+				@Override
+				public void onCancel() {
+					// TODO Auto-generated method stub
+					Toast.makeText(SigninActivity.this,"Canceled",Toast.LENGTH_SHORT).show();
+				}
+			});
+		}
+		
+	}
+		
+	}
 
 	
 	
